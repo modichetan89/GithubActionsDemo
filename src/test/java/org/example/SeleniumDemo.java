e0d67b3384cd28a7f608da312f3e1734d6d49cad
Chetan <modichetan89@gmail.com>
2023-06-09 20:45:58 +0530

    Initial commit

---
+++ src/test/java/org/example/SeleniumDemo.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SeleniumDemo {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test(priority = 1)
+    public void navigateToHomePage(){
+        driver.navigate().to("https://www.amazon.in");
+    }
+
+    @Test(priority = 2)
+    public void getHomePageTitle(){
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        String expectedTitle = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+}
